3e5352515cd80f9004d8510fe20c3d708e0959ab
Anthony Brochu <anthony.brochu@outlook.com>
2022-02-08 13:36:37 -0500

    comment mongo for now

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,12 @@ import { MongooseModule } from '@nestjs/mongoose';
     ConfigModule.forRoot({
         isGlobal: true
     }),
-    MongooseModule.forRootAsync({
-        useFactory: async (configService: ConfigService) => ({
-            uri: configService.get('MONGO_URI')
-        }),
-        inject: [ConfigService]
-    }),
+    // MongooseModule.forRootAsync({
+    //     useFactory: async (configService: ConfigService) => ({
+    //         uri: configService.get('MONGO_URI')
+    //     }),
+    //     inject: [ConfigService]
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
