0ea89b2c9796ae920eb58e8f27bd34ce80ffdcb1
Anthony Brochu <anthony.brochu@outlook.com>
2022-02-28 18:02:09 -0500

    fixed for unit test

--- src/nfticket-transaction/nfticket-transaction.controller.spec.ts
+++ src/nfticket-transaction/nfticket-transaction.controller.spec.ts
@@ -1,18 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NfticketTransactionController } from './nfticket-transaction.controller';
+import { NfticketTransactionModule } from './nfticket-transaction.module';
+import { NfticketTransactionService } from './nfticket-transaction.service';
 
 describe('NfticketTransactionController', () => {
   let controller: NfticketTransactionController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [NfticketTransactionModule],
       controllers: [NfticketTransactionController],
+      providers: [NfticketTransactionService]
     }).compile();
 
     controller = module.get<NfticketTransactionController>(NfticketTransactionController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(controller.getHello()).toBe('Hello World!');
+    });
   });
 });
