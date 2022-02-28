d7f1b928526e2b8434862357ff3ebf71fe6f8edc
Anthony Brochu <anthony.brochu@outlook.com>
2022-02-28 13:32:22 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  console.log(join(__dirname, 'schemas' ))
-  app.useStaticAssets(join(__dirname, 'schemas'));
   await app.listen(3000);
 }
 bootstrap();
